ba08aa62a181846ab38cb59cb9ac988d051031ce
arecaps <arecaplan@gmail.com>
2013-08-14 22:05:07 -0400

    extending

---
+++ extend.php
@@ -0,0 +1,24 @@
+<?php
+  $obj = new transactions();
+
+print_r($obj);
+
+  class fileOperations {
+    public $filename = 'file.csv';
+    public$directory = 'uploads';
+
+    public function __construct($filename = NULL, $directory = NULL) {
+      if($filename != NULL) {
+        $this->filename = $filename;
+      }
+      if(!is_NULL($directory) ) {
+        $this->directory = $directory;
+      }
+    }
+  }
+
+  class transactions extends fileOperations {
+
+  }
+
+?>
