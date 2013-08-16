2d2eb2e4e4c13ec998b7a27f70f7d3e312a90970
arecaps <arecaplan@gmail.com>
2013-08-16 15:43:39 -0400

    made some fixes

--- loop1.php
+++ loop1.php
@@ -2,19 +2,17 @@
 
   class squares {
 // set variable
-  public function upperLimit() {
+  public function __construct ($upperLimit) {
     $this->upperLimit = $upperLimit;
     $lowerLimit = 1;
-  }
-print_r($upperLimit);
-// keep printing squares until lower limit = upper limit
-    while ($lowerLimit <= $upperLimit) {
+  // keep printing squares until lower limit = upper limit
+    while ($lowerLimit <= $upperLimit) 
       echo ($lowerLimit * $lowerLimit).'<br>';
 
       $lowerLimit++;
   } 
 }
-$obj = new squares();
+$obj = new squares(100);
 
 echo 'END';
 
