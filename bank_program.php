b45eb8a13ff4e0a2ad72ce83a80c10784ec55330
arecaps <arecaplan@gmail.com>
2013-08-21 09:18:30 -0400

    some html

---
+++ bank_program.php
@@ -0,0 +1,33 @@
+<html>
+  <head>
+    <title>Bank Program</title>
+      <style type="text/css">
+        body {
+		font-family:Verdana,arial,sans-serif;
+		font-size:10pt;
+		margin:30px;
+		background-color:#ffcc00;
+		}
+		span {
+		font-family: Impact;
+		}
+		body{
+		text-align:center;
+		}
+		
+	  </style> 
+		<div id="wrapper" style="width:100%; text-align:center">
+		 <img src="http://farm6.staticflickr.com/5104/5635223299_82decd38ef.jpg" width="500" height="357" alt="Bank"></a>
+		</div>
+  </head>
+    <body>
+      <h1>Welcome to <span>The Bank</span></h1>
+	  <h2>We welcome new customers </h2>
+		<ul>
+			<li><a href="put link here">Open a new account</a></li>
+			<li><a href="put link here">Log in to your account</a></li>
+		</ul>
+          
+	</body>
+</html>
+ 
