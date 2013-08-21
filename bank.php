192c6701bfa1a987e324b5fadcc7eb4fa00e5b61
arecaps <arecaplan@gmail.com>
2013-08-20 23:38:42 -0400

    blueprint

---
+++ bank.php
@@ -0,0 +1,10 @@
+<?php
+//First need to make a sigup/signin page - 1.Need to set a variable to zero or false to detirmine the begining of a session.
+//2. Need to make 2 href links to either "log in" or "Sign Up"
+//3. for signup, need to create HTML form with fields to enter; firstName, LastName, UserName, AccountNumber, Password, and StartingBalance. Also need submit button.
+//4.need to set the form so the information is sent back to this file, so that it can be written to a .csv file.
+//For sign in option,1. need a form that has 2 feilds for UsserName and PassWord, and a submitt button. 
+//2. Need a methodn to send that info back to this file.
+//3.Need a function to open trhe .csv file and check for a match to the UserNameand if foun, to check for a match in the Password
+//4. Need a function to start a session to hold the connnection until logout.
+?>
