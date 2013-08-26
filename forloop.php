afcf5a55e2da32c96719c90ca2f7d7aa5ac45772
arecaps <arecaplan@gmail.com>
2013-08-26 01:08:08 +0000

    added continue

--- forloop.php
+++ forloop.php
@@ -1,6 +1,23 @@
 <?php
   
-   	  	for($var = 1;$var<50;$var ++) {
-   	  		echo $var. ", ";
-   	  	}
+   	for($var = 1;$var<50;$var ++) {
+	  if($var %2 ==1) {
+		continue;
+	  }	
+   	  	echo $var. ", ";
+   	}
+	echo "<br>";
+	for ($i=0; $i<=5; $i++) {
+	  if ($i % 2 ==0 ) {continue;}
+	    for ($k = 0; $k<=5; $k++) {
+		  echo $i. "-". $k. "<br>";
+		}
+    }
+	echo "<br>";
+	for($var = 1;$var<50;$var ++) {
+	  if($var ==24) {
+		break;
+	  }	
+   	  	echo $var. ", ";
+   	}
 ?>   	 
