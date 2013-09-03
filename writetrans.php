b18e29cb3e0c6d720765c8fb7e0d2acc4644dd00
arecaps <arecaplan@gmail.com>
2013-09-03 20:59:10 +0000

    got it working

--- writetrans.php
+++ writetrans.php
@@ -1,16 +1,20 @@
 <?php
-	$test = array ('amount' => 234, 'type' => 'debit' );
-	$sessn = array ('username' => 'arecaps' );
-		$name = $sessn['username'];
-		//print_r($name);
+$obj = new transactions;
+$obj->write_trans();
 		class transactions {
-			function write_trans() {
-				$fp = fopen("{$name}.csv", 'a');
-					foreach ($test as $fields) {
-					fputcsv($fp, $fields);
-					}
-					fclose($fp);	
-			
+			function write_trans() {	
+				$test = array ('amount' => 6098, 'type' => 'debit' );
+				$sessn = array ('username' => 'arecaps' );
+					$name = $sessn['username'];
+					$keys =array_keys($test);
+					$values=array_values($test);
+					$trans=array($keys, $values);
+					print_r($trans);
+					$fp = fopen("{$name}.csv", 'a');
+						foreach ($trans as $fields) {
+						fputcsv($fp, $fields);
+						}
+						fclose($fp);	
 			}
 		
 		}
